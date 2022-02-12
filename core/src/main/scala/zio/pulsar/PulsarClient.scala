8cc685a4fd16a8fd3474038522bf98ac273fa3c9
jczuchnowski <jakub.czuchnowski@gmail.com>
2022-02-12 23:23:53 +0100

    Add support for Testcontainers and a basic test

--- core/src/main/scala/zio/pulsar/PulsarClient.scala
+++ core/src/main/scala/zio/pulsar/PulsarClient.scala
@@ -8,14 +8,17 @@ trait PulsarClient:
 
 object PulsarClient:
 
-  def live(host: String, port: Int): ULayer[PulsarClient] =
-    val builder = JPulsarClient.builder().serviceUrl(s"pulsar://$host:$port")
+  def live(url: String): ULayer[PulsarClient] =
+    val builder = JPulsarClient.builder().serviceUrl(url)
   
     val cl = new PulsarClient {
       val client = ZIO.effect(builder.build).refineToOrDie[PulsarClientException]
     }
   
     ZManaged.make(ZIO.effectTotal(cl))(c => c.client.map(_.close()).orDie).toLayer
+
+  def live(host: String, port: Int): ULayer[PulsarClient] =
+    live(s"pulsar://$host:$port")
 
   def make: ZIO[PulsarClient, PulsarClientException, JPulsarClient] =
     ZIO.accessM[PulsarClient](_.get.client)
